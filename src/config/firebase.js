d7cc5aea4c2b654e0e37e47753671d1be9cb0e0c
gvarlasalle <gampueror@ulasalle.edu.pe>
2025-11-19 23:04:49 -0500

    configuraciones

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,18 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGlnRMr14mETe9UAdQ-RlmFbaIdS8N3J4",
-  authDomain: "thesistrack-d552a.firebaseapp.com",
-  projectId: "thesistrack-d552a",
-  storageBucket: "thesistrack-d552a.firebasestorage.app",
-  messagingSenderId: "174426043020",
-  appId: "1:174426043020:web:82c7337f597fc0a7b001a9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
